1f174e9cc37b4a66fc357dae8f64fa018683bc85
LAM HINE YAN Randy Harry Narovana <198113444+Narovana69@users.noreply.github.com>
2026-05-10 03:29:37 +0200

    Modif config base

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
+        'hostname'     => '127.0.0.1',
         'username'     => '',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'regime_alimentaire',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
